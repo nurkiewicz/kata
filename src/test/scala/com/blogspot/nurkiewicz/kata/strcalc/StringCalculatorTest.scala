e9a2322b7ce45bf633b6d9908b37b7762fead1b0
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2012-04-06 23:04:20 +0200

    Tests for step 1 (red)

--- src/test/scala/com/blogspot/nurkiewicz/kata/strcalc/StringCalculatorTest.scala
+++ src/test/scala/com/blogspot/nurkiewicz/kata/strcalc/StringCalculatorTest.scala
@@ -11,5 +11,32 @@ import StringCalculator.add
 
 class StringCalculatorTest extends org.scalatest.FunSuite with ShouldMatchers {
 
+	test("should return 0 for empty string") {
+		add("") should equal (0)
+	}
+
+	test("should return 0 when only 0 as input") {
+		add("0") should equal (0)
+	}
+
+	test("should return single provided value") {
+		add("42") should equal (42)
+	}
+
+	test("should return single negative value") {
+		add("-42") should equal (-42)
+	}
+
+	test("should return sum of two values") {
+		add("2, 3") should equal (5)
+	}
+
+	test("should return sum of two values where second is negative") {
+		add("2, -3") should equal (-1)
+	}
+
+	test("should return 0 when positive and negative values provided with same absolute value") {
+		add("-4, 4") should equal (0)
+	}
 
 }
