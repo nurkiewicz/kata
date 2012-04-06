cac7e5c6a3b3a49533de486ad0bd21b684eb6717
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2012-04-06 23:39:09 +0200

    Step 1 green (and dirty)

--- src/main/scala/com/blogspot/nurkiewicz/kata/strcalc/StringCalculator.scala
+++ src/main/scala/com/blogspot/nurkiewicz/kata/strcalc/StringCalculator.scala
@@ -9,10 +9,23 @@ object StringCalculator {
 
 	val NumbersDelimiter = """(,\s*)|\\n"""
 
-	def add(numbers: String) = numbers.
-			split(NumbersDelimiter).
-			filterNot(_.isEmpty).
-			map(_.toInt).
-			sum
+	def add(numbers: String) = 
+		if(numbers startsWith "//") {
+			val (delimiter :: input) = numbers.lines.toList
+			input match {
+				case nums :: _ => nums.
+					split(delimiter.substring(2)).
+							filterNot(_.isEmpty).
+							map(_.toInt).
+							sum
+				case Nil => 0
+			}
+		} else {
+			numbers.
+				split(NumbersDelimiter).
+				filterNot(_.isEmpty).
+				map(_.toInt).
+				sum
+		}
 
 }
