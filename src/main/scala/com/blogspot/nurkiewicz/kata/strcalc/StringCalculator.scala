77094ac5975c13c7d655601a38ab5ddf75db9de1
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2012-04-06 23:03:54 +0200

    TDD Kata 1 - String Calculator - Osherove (http://osherove.com/tdd-kata-1/) - stubs

---
+++ src/main/scala/com/blogspot/nurkiewicz/kata/strcalc/StringCalculator.scala
@@ -0,0 +1,12 @@
+package com.blogspot.nurkiewicz.kata.strcalc
+
+/**
+ * @author Tomasz Nurkiewicz
+ * @since 06.04.12, 22:50
+ */
+
+object StringCalculator {
+
+	def add(numbers: String) = 0
+
+}
---
+++ src/test/scala/com/blogspot/nurkiewicz/kata/strcalc/StringCalculatorTest.scala
@@ -0,0 +1,15 @@
+package com.blogspot.nurkiewicz.kata.strcalc
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import StringCalculator.add
+
+/**
+ * @author Tomasz Nurkiewicz
+ * @since 06.04.12, 22:51
+ */
+
+class StringCalculatorTest extends org.scalatest.FunSuite with ShouldMatchers {
+
+
+}
